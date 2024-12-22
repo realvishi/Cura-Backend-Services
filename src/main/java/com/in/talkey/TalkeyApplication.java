ae3cad8d839806dfde5bb0cf3b208e3d0bb0ec69
Vishal V <vishal.varshney@veersatech.com>
2024-12-22 14:07:12 +0530

    docker updated

--- src/main/java/com/in/talkey/TalkeyApplication.java
+++ src/main/java/com/in/talkey/TalkeyApplication.java
@@ -4,7 +4,9 @@ import com.in.talkey.entity.Users;
 import org.apache.catalina.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages = "com.in.talkey.repository")
 @SpringBootApplication
 public class TalkeyApplication {
 
